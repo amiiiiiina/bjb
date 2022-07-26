a1a3fecd3d87481b2896c432cb1f408a06892080
ImIlusha <thebest.ilya.04@mail.ru>
2022-07-26 15:23:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function moneyConverter(vaLNum) {
+    document.getElementById('outputsum').innerHTML = vaLNum * 22.17
+}
+
+
+function moneyConverter1(vaRNum) {
+    document.getElementById('outputtenge').innerHTML = vaRNum * 0.17
+}
